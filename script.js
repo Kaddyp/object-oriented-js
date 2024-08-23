de72827ab5d61d1f16432aa6bbe25c49bc3762d1
Kaddyp <nitin.pawar@yondrgroup.com>
2024-08-23 17:09:20 +0100

    Classes in JS

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-var user = {
-    email: 'diyan@gmail.com',
-    name: 'Diyan',
-    login(){
-        console.log(this.email, 'has logged in');
-    },
-    logout(){
-        console.log(this.email, 'has logged out');
-    }
-};
-
-user.login();
-user.logout();
-
-var prop = 'name'
-
-// user.email = 'kalyani@gmail.com';
-// user['email'] = 'kalyani@gmail.com';
-// user[prop] = 'Kaddy';
-
-// console.log(user);
-
-user.age = 25;
-user.logInfo = function(){
-    console.log(this.name, this.age, this.email);
-};
-
-user.logInfo();
-user.login();
-user.logout();
+class User {
+    //empty class
+}
