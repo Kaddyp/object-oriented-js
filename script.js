3835803885cec0312270722c3dfe4d5787f85c0e
Kaddyp <nitin.pawar@yondrgroup.com>
2024-08-23 17:20:26 +0100

    Class Constructors in JS.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 class User {
-    //empty class
+    //constructor function to create a new User object
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+    }
 }
+
+var userOne = new User('kaddy@gmail.com','Kaddy');
+var userTwo = new User('diyan@gmail.com','Diyan');
+
+console.log(userOne); 
+
+// the 'new' keyword 
+// - creates a new empty object{}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor function or method
