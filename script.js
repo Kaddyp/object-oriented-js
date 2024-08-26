87864e84b771df732b008470ea2fb9e02c2e8d78
Kaddyp <nitin.pawar@yondrgroup.com>
2024-08-26 22:55:10 +0100

    Method Chaining in JS.

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ class User {
     constructor(email, name){
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     login(){
         console.log(this.email, 'just logged in');
+        return this; //return this to allow method chaining
     }
     logout(){
         console.log(this.email, 'just logged out');
+        return this; //return this to allow method chaining
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'updated score to', this.score);
+        return this; //return this to allow method chaining
     }
 }
 
 var userOne = new User('kaddy@gmail.com','Kaddy');
 var userTwo = new User('diyan@gmail.com','Diyan');
 
-//console.log(userOne); 
-//console.log(userTwo); 
 
-userOne.login();
-userTwo.logout();
+//userOne.login().logout(); //Error because return this is required then we can use method Chaining
+//userOne.logout();
+//userTwo.logout();
+// This is call method chaining
+userOne.login().updateScore().updateScore().logout();
