200e0e9ef6d03a387f4e5c4c14f89f84fc318d7a
Kaddyp <nitin.pawar@yondrgroup.com>
2024-08-27 01:39:48 +0100

    Prototype Inheritance in JS.

--- script.js
+++ script.js
@@ -13,8 +13,24 @@ User.prototype.logout = function(){
     console.log(this.email, 'just logged out'); 
 }
 
+function Admin(...args) {
+    //console.log(args);
+    User.apply(this, args); //inherit properties form User
+    this.role = 'Super admin';
+}
+//Inherit function
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.deleteUser = function(u){
+    users = users.filter(user => {
+        return user.email != u.email;
+    });
+}
+
 var userOne = new User('kaddy@gmail.com','Kaddy');
 var userTwo = new User('diyan@gmail.com','Diyan');
+var admin = new Admin('admin@gmail.com','Admin');
+var users = [userOne, userTwo, admin];
 
-console.log(userOne);
-userTwo.login();
+//admin.login();
+admin.deleteUser(users[1]);
+console.log(users);
