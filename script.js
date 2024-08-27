7cd71ae497f3c1c1ae370202c6dfa8040879b42f
Kaddyp <nitin.pawar@yondrgroup.com>
2024-08-27 01:18:29 +0100

    Prototype in JS.

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ function User(email, name) {
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function(){
-        console.log(this.email, 'just logged in');       
-    }
-    this.logout = function(){
-        console.log(this.email, 'just logged out');       
-    }
 }
 
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'just logged in');
+}
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email, 'just logged out'); 
+}
 
 var userOne = new User('kaddy@gmail.com','Kaddy');
 var userTwo = new User('diyan@gmail.com','Diyan');
